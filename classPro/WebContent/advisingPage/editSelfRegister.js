1cdfec1ca8bce1eebf596e38ae5dd2e12845934d
LizaMarie <lizamariejimenez@gmail.com>
2020-03-05 10:34:33 -0500

    tried to create js but did not work script did. and working on adding to form

---
+++ classPro/WebContent/advisingPage/editSelfRegister.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+function addClass() {
+	var container = document.getElementById("container");
+	var input = document.createElement("input");
+
+
+	container.appendChild(document.createTextNode("Member"));
+	input.type = "text";
+	container.appendChild(input);
+	container.appendChild(document.createElement("br"));
+	
+	document.getElementById("className").innerHTML='this is working in out of script';
+
+}
